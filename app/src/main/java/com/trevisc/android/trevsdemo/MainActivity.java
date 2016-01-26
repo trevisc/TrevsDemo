b3a406e351e67c06cff0e35db22c4ef8ee05ee6c
Trevis <trevisc@yahoo.com>
2016-01-25 18:47:47 -0600

    Initial commit

---
+++ app/src/main/java/com/trevisc/android/trevsdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.trevisc.android.trevsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
